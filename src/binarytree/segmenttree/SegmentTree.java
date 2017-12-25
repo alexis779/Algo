e327ac65bc3acd4ab43e28745a6ca52c91410a17
Alexis <alexis779@hotmail.com>
2017-12-24 21:03:36 -0800

    Range Update and Lazy Propagation in Segment Tree

--- src/binarytree/segmenttree/SegmentTree.java
+++ src/binarytree/segmenttree/SegmentTree.java
@@ -1,207 +1,237 @@
 package binarytree.segmenttree;
 
 /**
- * Range Minimum Query
+ * Range Minimum Query.
  */
 public abstract class SegmentTree<T> {
-	/**
-	 * Number of elements contained in the original array 
-	 */
-	int n;
-	/**
-	 * 1-indexed original array
-	 */
-	T[] a;
-	
-	/**
-	 * Number of nodes in the binary tree
-	 */
-	int size;
-	/**
-	 * 1-indexed array representing the binary tree
-	 */
-	T[] nodes;
-	/**
-	 * Height of the tree
-	 */
-	int height;
-	
-	SegmentTree(T[] a) {
-		this.a = a;
-		this.n = this.a.length-1;
-		build();
-	}
-	
-	private void build() {
-		int k = msb(n);
-		height = k;
-		int start = 1 << k;
-		
-		// number of leaves at the level k+1
-		int leaves1 = (n - start) << 1;
-		start <<= 1;
-		size = start + leaves1 - 1;
+    /**
+     * Number of elements in the original array
+     */
+    int n;
 
-		// array is 1-indexed
-		nodes = (T[]) new Object[size+1];
-		
-		// populate the leaves at the level k+1
-		for (int i = 1; i <= leaves1; i++) {
-			nodes[start + i - 1] = a[i];
-		}
-		// populate the leaves at the level k
-		start = (start >> 1) + (leaves1 >> 1);
-		for (int i = leaves1+1; i <= n; i++) {
-			nodes[start + (i-leaves1) - 1] = a[i];
-		}
-		
-		// populate the internal nodes
-		for ( ; k >= 0; k--) {
-			// populate level k
-			for (int i = 1 << k; i < 1 << (k+1); i++) {
-				// assign the result of the query of left and right to parent
-				int left = i << 1;
-				if (left <= size) {
-					nodes[i] = nodes[left];
-				}
-				int right = left+1;
-				if (right <= size) {
-					nodes[i] = query(nodes[i], nodes[right]);
-				}
-			}
-		}
-	}
+    /**
+     * 1-indexed original array
+     */
+    T[] a;
 
-	private int msb(int n) {
-		int k = 0;
-		while (n > 0) {
-			n >>= 1;
-			k++;
-		}
-		return k-1;
-	}
-	
-	/**
-	 * @param i left bound of the interval
-	 * @param j	right bound of the interval
-	 * @return the result of the range query in interval [i-j]
-	 */
-	public T rangeQuery(int i, int j) {
-		int start = 1 << height;
-		int leaves1 = (this.n - start) << 1;
-		
-		T result = neutralElement();
-		
-		int first = start + (leaves1 >> 1);
-		int a = Math.max(first + i - leaves1 - 1, first);
-		int b = (start << 1) - 1 - (n-j);
-		if (a <= b && first <= b) {
-			// search leaves at level k
-			result = search(a, b, 1, height);
-		}
-		
-		if (leaves1 != 0) {
-			start <<= 1;
-			
-			int last = start + leaves1 - 1;
-			a = start + i - 1;
-			b = Math.min(start + j - 1, last);
-			if (a <= b && a <= last) {
-				// search leaves at level k+1
-				result = query(result, search(a, b, 1, height+1));
-			}
-		}
+    /**
+     * 1-indexed array representing the binary tree
+     */
+    T[] nodes;
 
-		return result;
-	}
+    /**
+     * 1-indexed array indicating there is a pending update
+     */
+    boolean[] pending;
+    /**
+     * 1-indexed array for pending update value
+     */
+    T[] update;
 
-	private T search(int i, int j, int parent, int depth) {
-		int a = parent << depth;
-		int b = a + (1 << depth) - 1;
-		
-		if (i == a && j == b) {
-			return nodes[parent];
-		}
-		
-		int m = (a+b)/2;
-		
-		int left = parent << 1;
-		int right = left+1;
-		if (j <= m) {
-			return search(i, j, left, depth-1);
-		} else if (i > m) {
-			return search(i, j, right, depth-1);
-		} else {
-			return query(search(i, m, left, depth-1), search(m+1, j, right, depth-1));
-		}
-	}
+    public SegmentTree(T[] a) {
+        this.a = a;
+        this.n = this.a.length - 1;
+        build();
+    }
 
-	public abstract T query(T x, T y);
+    @SuppressWarnings("unchecked")
+    private void build() {
+        // height of the tree
+        int height = msb(n)+1;
+        // size of the tree
+        int size = 1 << height;
 
-	public abstract T neutralElement();
+        nodes = (T[]) new Object[size];
 
-	public void print() {
-		for (int i = 1; i < nodes.length; i++) {
-			System.out.print(nodes[i] + " ");
-		}
-		System.out.println();
-		print(1, 0);
-	}
+        update = (T[]) new Object[size];
+        pending = new boolean[size];
+        for (int i = 1; i < size; i++) {
+            update[i] = neutralElement();
+        }
+        build(1, 1, n);
+    }
 
-	private void print(int parent, int depth) {
-		int a = (parent << (height+1-depth));
-		int b = a + (1 << (height+1-depth)) - 1;
-		for (int i = 0; i < depth; i++) {
-			System.out.print(" ");
-		}
-		int internalNodes = (1 << (height+1)) - 1;
-		System.out.println(nodes[parent] + " [" + (a - internalNodes) + "," + (b - internalNodes) + "]");
-		
-		int left = parent << 1;
-		if (left <= size) {
-			print(left, depth+1);
-		}
-		
-		int right = left+1;
-		if (right <= size) {
-			print(right, depth+1);
-		}
-	}
+    private int msb(int n) {
+        int k = 0;
+        while (n > 0) {
+            n >>= 1;
+            k++;
+        }
+        return k;
+    }
 
-	/**
-	 * Set value to v at position i.
-	 *
-	 * @param pos the position to update
-	 * @param v the new value
-	 */
-	public void update(int pos, T v) {
-		a[pos] = v;
+    private void build(int i, int start, int end) {
+        if (start >= end) {
+            nodes[i] = a[start];
+            return;
+        }
 
-		int start = 1 << height;
-		int leaves1 = (this.n - start) << 1;
-		int i;
-		if (pos <= leaves1) {
-			// level k+1
-			i = (start << 1) + pos - 1;
-		} else {
-			// level k
-			int first = start + (leaves1 >> 1);
-			i = first + pos - leaves1 - 1;
-		}
+        int mid = middle(start, end);
+        int left = i << 1;
+        int right = left + 1;
 
-		nodes[i] = v;
-		while(i > 1) {
-			int parent = i >> 1;
-			if (i % 2 == 1) {
-				i--;
-			}
-			T parentValue = query(nodes[i], nodes[i+1]);
-			if (! parentValue.equals(nodes[parent])) {
-				nodes[parent] = parentValue;
-			} else {
-				break;
-			}
-			i = parent;
-		}
-	}
+        build(left, start, mid);
+        build(right, mid + 1, end);
+        nodes[i] = query(nodes[left], nodes[right]);
+    }
+
+    private int middle(int start, int end) {
+        return start + (end - start) / 2;
+    }
+
+    /**
+     * @param i left bound of the interval
+     * @param j right bound of the interval
+     * @return the result of the range query in interval [i,j]
+     */
+    public T rangeQuery(int i, int j) {
+        assert 1 <= i && i <= j && j <= n;
+        return search(1, 1, n, i, j);
+    }
+
+    private T search(int i, int nodeStart, int nodeEnd, int start, int end) {
+        // check if there are pending updates to apply
+        if (pending[i]) {
+            updatePending(i, nodeStart, nodeEnd, neutralElement());
+        }
+
+        if (nodeStart == start && nodeEnd == end) {
+            return nodes[i];
+        }
+
+        int mid = middle(nodeStart, nodeEnd);
+        int left = i << 1;
+        int right = left+1;
+
+        if (start <= mid) {
+            if (end <= mid) {
+                T leftValue = search(left, nodeStart, mid, start, end);
+                return leftValue;
+            } else {
+                T leftValue = search(left, nodeStart, mid, start, mid);
+                T rightValue = search(right, mid+1, nodeEnd, mid+1, end);
+                return query(leftValue, rightValue);
+            }
+        } else {
+            T rightValue = search(right, mid+1, nodeEnd, start, end);
+            return rightValue;
+        }
+    }
+
+    private void updatePending(int i, int nodeStart, int nodeEnd, T diff) {
+        diff = query(diff, update[i]);
+
+        pending[i] = false;
+        update[i] = neutralElement();
+
+        if (nodeStart >= nodeEnd) {
+            a[nodeStart] = query(a[nodeStart], diff);
+            nodes[i] = a[nodeStart];
+            return;
+        }
+
+        int mid = middle(nodeStart, nodeEnd);
+        int left = i << 1;
+        int right = left+1;
+        if (pending[left] || ! diff.equals(neutralElement())) {
+            updatePending(left, nodeStart, mid, diff);
+        }
+        if (pending[right] || ! diff.equals(neutralElement())) {
+            updatePending(right, mid + 1, nodeEnd, diff);
+        }
+        nodes[i] = query(nodes[left], nodes[right]);
+    }
+
+    /**
+     * Set value to v at position i.
+     *
+     * @param pos the position to update
+     * @param v   the new value
+     */
+    public void update(int pos, T v) {
+        rangeQuery(pos, pos);
+        update(1, 1, n, pos, v);
+    }
+
+    private void update(int i, int nodeStart, int nodeEnd, int pos, T v) {
+        if (nodeStart == pos && nodeEnd == pos) {
+            a[pos] = v;
+            nodes[i] = a[pos];
+            return;
+        }
+
+        int mid = middle(nodeStart, nodeEnd);
+        int left = i << 1;
+        int right = left+1;
+
+        if (pos <= mid) {
+            update(left, nodeStart, mid, pos, v);
+        } else {
+            update(right, mid+1, nodeEnd, pos, v);
+        }
+        nodes[i] = query(nodes[left], nodes[right]);
+    }
+
+    public void rangeUpdate(int start, int end, T diff) {
+        rangeUpdate(1, 1, n, start, end, diff);
+    }
+
+    /**
+     * Use Lazy Propagation for range update operation.
+     *
+     * @param i
+     * @param nodeStart
+     * @param nodeEnd
+     * @param start
+     * @param end
+     * @param diff
+     */
+    private void rangeUpdate(int i, int nodeStart, int nodeEnd, int start, int end, T diff) {
+        pending[i] = true;
+        if (nodeStart == start && nodeEnd == end) {
+            // set the node pending update
+            update[i] = query(update[i], diff);
+            return;
+        }
+
+        int mid = middle(nodeStart, nodeEnd);
+        int left = i << 1;
+        int right = left+1;
+
+        if (start <= mid) {
+            if (end <= mid) {
+                rangeUpdate(left, nodeStart, mid, start, end, diff);
+            } else {
+                rangeUpdate(left, nodeStart, mid, start, mid, diff);
+                rangeUpdate(right, mid+1, nodeEnd, mid+1, end, diff);
+            }
+        } else {
+            rangeUpdate(right, mid+1, nodeEnd, start, end, diff);
+        }
+    }
+
+    public void print() {
+        print(1, 1, n, 0);
+    }
+
+    private void print(int i, int nodeStart, int nodeEnd, int depth) {
+        for (int j = 0; j < depth; j++) {
+            System.out.print(" ");
+        }
+        System.out.println(String.format("%s [%d-%d]", nodes[i], nodeStart, nodeEnd));
+
+        if (nodeStart >= nodeEnd) {
+            return;
+        }
+
+        int mid = middle(nodeStart, nodeEnd);
+        int left = i << 1;
+        int right = left+1;
+
+        print(left, nodeStart, mid, depth+1);
+        print(right, mid+1, nodeEnd, depth+1);
+    }
+
+    public abstract T neutralElement();
+    public abstract T query(T x, T y);
 }
